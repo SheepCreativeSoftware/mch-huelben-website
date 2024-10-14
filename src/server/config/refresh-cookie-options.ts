49ebd1622f54dca554efd8b500dc9c8fe2377cf0
magraina <marinafcegner@gmail.com>
2024-10-14 22:10:22 +0200

    fix: wrong expiration for refresh token cookie

--- src/server/config/refresh-cookie-options.ts
+++ src/server/config/refresh-cookie-options.ts
@@ -2,9 +2,9 @@ import type { CookieOptions } from 'express';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-// 5 days
+// 1 days
 // eslint-disable-next-line no-magic-numbers -- This is a constant
-const EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 5;
+const EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 const getRefreshCookieOptions = (): CookieOptions => ({
 	domain: process.env.HOST,
 	expires: new Date(Date.now() + EXPIRATION_TIME),
