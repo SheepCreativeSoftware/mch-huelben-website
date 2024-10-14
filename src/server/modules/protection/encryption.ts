4c3ab80f8f6a7594ac58d5045e08ef36339d22ba
magraina <marinafcegner@gmail.com>
2024-10-15 00:24:11 +0200

    fix: refresh token  could not be created

--- src/server/modules/protection/encryption.ts
+++ src/server/modules/protection/encryption.ts
@@ -4,11 +4,14 @@ import { ForbiddenException } from '../misc/custom-errors.js';
 const KEY_BYTES = 32;
 const VECTOR_BYTES = 16;
 const algorithm = 'aes-256-cbc';
-const key = process.env.SESSION_SECRET ?? crypto.randomBytes(KEY_BYTES);
+const key = process.env.SESSION_SECRET;
+const hashedKey = crypto.createHash('sha256').update(String(key)).digest();
+if (hashedKey.length !== KEY_BYTES) throw new Error('Key length must be 32 bytes');
 
 const encryptData = (data: string): string => {
 	const initVector = crypto.randomBytes(VECTOR_BYTES);
-	const cipher = crypto.createCipheriv(algorithm, key, initVector);
+
+	const cipher = crypto.createCipheriv(algorithm, hashedKey, initVector);
 
 	let encryptedData = cipher.update(data, 'utf8');
 	encryptedData = Buffer.concat([encryptedData, cipher.final()]);
@@ -21,7 +24,7 @@ const decryptData = (encryptedPayload: string) => {
 		const initVector = encryptedPayloadBuffer.subarray(0, VECTOR_BYTES);
 		const encryptedData = encryptedPayloadBuffer.subarray(VECTOR_BYTES, encryptedPayloadBuffer.length);
 
-		const decipher = crypto.createDecipheriv(algorithm, key, initVector);
+		const decipher = crypto.createDecipheriv(algorithm, hashedKey, initVector);
 
 		// eslint-disable-next-line no-undefined -- no input encoding needed as input is buffer
 		let decrypted = decipher.update(encryptedData, undefined, 'utf8');
