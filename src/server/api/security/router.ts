0fdecc5ab0fd6421fde04c7da4edb7e8d5120da4
magraina <marinafcegner@gmail.com>
2024-10-16 12:38:11 +0200

    feat: add file upload

--- src/server/api/security/router.ts
+++ src/server/api/security/router.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { getLogoutUserHandle } from './logout-user/handle.js';
 import { getRefreshTokenHandle } from './refresh-token/handle.js';
 import { loginUserHandle } from './login-user/handle.js';
+import { userAuthorizedHandler } from '../../modules/protection/user-auth-check.js';
 
 // eslint-disable-next-line new-cap -- This is not a constructor
 const router = express.Router();
 
 router.post('/login-user', loginUserHandle());
-router.post('/logout-user', getLogoutUserHandle());
-router.post('/refresh-token', getRefreshTokenHandle());
+router.post('/logout-user', userAuthorizedHandler(), getLogoutUserHandle());
+router.post('/refresh-token', userAuthorizedHandler(), getRefreshTokenHandle());
 
 export { router as securityRouter };
